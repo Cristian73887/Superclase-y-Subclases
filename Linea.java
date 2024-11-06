76272b7a905641b1bd3c12cfa1a568cabdbe557b
Cristian73887 <cristianzamora211@gmail.com>
2024-11-06 00:43:56 -0600

    Subclase Línea
    La clase Línea también extiende Formas y representa una línea. Según el diagrama, solo tiene una propiedad llamada largo, sin métodos adicionales específicos.
    
    •	Propiedad larga: Representa la longitud de la línea.
    
    •	Método dibujar (): Sobrescribe el método dibujar () de la clase Formas para indicar que se está dibujando una línea.

---
+++ Linea.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author crist
+ */
+public class Linea extends Formas {
+      double largo;
+
+    @Override
+    public void dibujar() {
+        System.out.println("Dibujando una Línea.");
+    }
+}
+
+
