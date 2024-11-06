8b5ed8943196a18ea166ae544e2d6ad2bbcff30d
Cristian73887 <cristianzamora211@gmail.com>
2024-11-06 00:45:12 -0600

    Subclase Triangulo
    La clase Triangulo representa una forma triangular y extiende Formas. Según el diagrama, tiene una propiedad ángulo y un método específico calcular Área ().
    
    •	Propiedad ángulo: Representa un ángulo de los triángulos.
    
    •	Método dibujar (): Sobrescribe el método dibujar () de la clase Formas para indicar que se está dibujando un triángulo.
    
    •	Método calcular Área (): Método específico que imprime un mensaje indicando que está calculando el área del triángulo.

---
+++ Triangulo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author crist
+ */
+public class Triangulo extends Formas {
+    double angulo;
+
+    @Override
+    public void dibujar() {
+        System.out.println("Dibujando un Triángulo.");
+    }
+
+    public void calcularArea() {
+        System.out.println("Calculando el área del Triángulo.");
+    }
+
+}
