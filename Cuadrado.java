6b3c219ee27b151b50557b0e4e8fe4d0804aa123
Cristian73887 <cristianzamora211@gmail.com>
2024-11-06 00:46:11 -0600

    Subclase Cuadrado
    La clase Cuadrado extiende Formas y representa una forma cuadrada. Según el diagrama, tiene una propiedad área y un método específico calcular Área ().
    
    •	Propiedad área: Representa el área del cuadrado.
    
    •	Método dibujar (): Sobrescribe el método dibujar () de la clase Formas para indicar que se está dibujando un cuadrado.
    
    •	Método calcular Área (): Método específico que imprime un mensaje indicando que está calculando el área del cuadrado.

---
+++ Cuadrado.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author crist
+ */
+public class Cuadrado extends Formas {
+     double area;
+
+    @Override
+    public void dibujar() {
+        System.out.println("Dibujando un Cuadrado.");
+    }
+
+    public void calcularArea() {
+        System.out.println("Calculando el área del Cuadrado.");
+    }
+
+}
